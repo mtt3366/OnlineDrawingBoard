630ad195f709b998053c718e97586e8e9e1bf2f4
mataotao <1030557028@qq.com>
2018-04-24 20:38:45 +0800

    美化画笔,橡皮按钮,添加垃圾桶按钮

--- main.js
+++ main.js
@@ -17,56 +17,58 @@ listenToUser(canvas);
 
 /**
  * 理清  用画笔,用橡皮的思路
- * 1. 首先画笔是隐藏的,最开始只有橡皮
- * 2. 点击橡皮,(1)isUsingEraser = true,(2)画笔出现橡皮消失
+ * 1. 最开始画笔放大,有颜色
+ * 2. 点击橡皮,(1)isUsingEraser = true,(2)
  * 2. 点击画笔,(1)isUsingEraser = false,(2)橡皮出现画笔消失
  */
 var eraser = document.getElementById('eraser');
 var brush = document.getElementById("brush");
 var actions = document.getElementById('actions');
 
-if(document.body.ontouchstart === undefined){
-    eraser.onclick = function(){
+if (document.body.ontouchstart === undefined) {
+    eraser.onclick = function () {
         isUsingEraser = true;
-        actions.className = 'actions usingeraser';
+        eraser.classList.add("active");
+        brush.classList.remove("active");
+
 
     };
     brush.onclick = function () {
         isUsingEraser = false;
-        actions.className = 'actions';
+        eraser.classList.remove("active");
+        brush.classList.add("active");
     };
-}else{
-    eraser.ontouchstart = function(){
+} else {
+    eraser.ontouchstart = function () {
         isUsingEraser = true;
-        actions.className = 'actions usingeraser';
-
+        eraser.classList.add("active");
+        brush.classList.remove("active");
     };
     brush.ontouchstart = function () {
         isUsingEraser = false;
-        actions.className = 'actions';
+        eraser.classList.remove("active");
+        brush.classList.add("active");
     };
 }
-
-
-
 
 
 // 函数
 //画线
-function drawLine(beginx,beginy,endx,endy,lineWidth) {//开始xy,结束xy,线宽度
+function drawLine(beginx, beginy, endx, endy, lineWidth) {//开始xy,结束xy,线宽度
     context.beginPath();
     context.lineWidth = lineWidth;//先声明线的长度,不然他直接画了
     context.fillStyle = 'black';
-    context.moveTo(beginx,beginy);
-    context.lineTo(endx,endy);
+    context.moveTo(beginx, beginy);
+    context.lineTo(endx, endy);
     context.stroke();
     context.closePath();
 }
+
 //画圈
-function drawCircle(x,y,radius) {//画圆函数 ,xy圆心,radius半径
+function drawCircle(x, y, radius) {//画圆函数 ,xy圆心,radius半径
     context.beginPath();
     context.fillStyle = 'black';
-    context.arc(x,y,radius,0,Math.PI*2);
+    context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill();
 }
 
@@ -77,6 +79,7 @@ function autoSetCanvasSize(canvas) {//自动设置canvas函数
     window.onresize = function (e) {
         setCanvasSize();
     };
+
     //设置canvas高宽为屏幕宽高函数
     function setCanvasSize() {
 
@@ -99,7 +102,7 @@ function listenToUser(canvas) {
      *方法二: document.body.ontouchstart === undefined
      *
      * 手机*/
-    if(document.body.ontouchstart === undefined){//不支持touch事件,说明是pc端
+    if (document.body.ontouchstart === undefined) {//不支持touch事件,说明是pc端
         /**
          * 理清是否用画板和是否用橡皮的逻辑
          * 首先画笔默认开启.isUsingEraser = false就是在用画笔,true就是在用橡皮擦
@@ -112,14 +115,14 @@ function listenToUser(canvas) {
             var x = e.clientX;
             var y = e.clientY;
             isUsingBoard = true;
-            if(isUsingEraser){//
-                context.clearRect(x-5,y-5,10,10);
-            }else{//不用橡皮,用画笔
+            if (isUsingEraser) {//
+                context.clearRect(x - 5, y - 5, 10, 10);
+            } else {//不用橡皮,用画笔
                 lastPoint = {//上一个点//设置成全局变量,好能接受的到
-                    'x':x,
-                    'y':y
+                    'x': x,
+                    'y': y
                 };
-                drawCircle(x,y,1);
+                drawCircle(x, y, 1);
             }
 
         };
@@ -127,19 +130,19 @@ function listenToUser(canvas) {
         canvas.onmousemove = function (e) {
             var x = e.clientX;
             var y = e.clientY;
-            if(isUsingBoard){
-                if(isUsingEraser){
-                    context.clearRect(x-5,y-5,10,10);
-                }else {//不用橡皮,用画笔
+            if (isUsingBoard) {
+                if (isUsingEraser) {
+                    context.clearRect(x - 10, y - 10, 20, 20);
+                } else {//不用橡皮,用画笔
                     newPoint = {//设置成全局变量,好能接受的到
-                        'x':x,
-                        'y':y
+                        'x': x,
+                        'y': y
                     };
                     // drawCircle(x,y,1);//半径是1px,直径就是2px
                     //实际上不需要这个圈,因为不管有没有这个圈,他都会连线.所以删掉这句代码,也不影响
 
                     //老点与新点连线
-                    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y,2);
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, 2);
                     lastPoint = newPoint;//把现在这个新点变为下一个的老点.
                 }
             }
@@ -154,14 +157,14 @@ function listenToUser(canvas) {
             var x = e.touches[0].clientX;
             var y = e.touches[0].clientY;
             isUsingBoard = true;
-            if(isUsingEraser){//
-                context.clearRect(x-5,y-5,10,10);
-            }else{
+            if (isUsingEraser) {//
+                context.clearRect(x - 10, y - 10, 20, 20);
+            } else {
                 lastPoint = {
-                    'x':x,
-                    'y':y
+                    'x': x,
+                    'y': y
                 };
-                drawCircle(x,y,1);
+                drawCircle(x, y, 1);
             }
         }
         canvas.ontouchmove = function (e) {
@@ -171,16 +174,16 @@ function listenToUser(canvas) {
 
             var x = e.touches[0].clientX;
             var y = e.touches[0].clientY;
-            if(isUsingBoard){
-                if(isUsingEraser){
-                    context.clearRect(x-5,y-5,10,10);
-                }else {
+            if (isUsingBoard) {
+                if (isUsingEraser) {
+                    context.clearRect(x - 5, y - 5, 10, 10);
+                } else {
                     newPoint = {
-                        'x':x,
-                        'y':y
+                        'x': x,
+                        'y': y
                     };
 
-                    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y,2);
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, 2);
                     lastPoint = newPoint;
                 }
             }
@@ -189,8 +192,9 @@ function listenToUser(canvas) {
             isUsingBoard = false;
         }
     }
-    
+
 }
+
 /*
 
 学习API
@@ -238,7 +242,7 @@ body.ontouchstart = function (ev) {
     ev.preventDefault();
 }
 clearcanvas.onclick = function (e) {
-    context.clearRect(0,0,body.clientWidth,body.clientHeight);
+    context.clearRect(0, 0, body.clientWidth, body.clientHeight);
     //
     // console.log(body.clientWidth);
     // console.log(body.clientHeight);
